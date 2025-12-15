e772854b7df48592f21e7ea1f928109eaeb7dac4
Andrew Gordon <gordon.ap@gmail.com>
2025-12-15 15:09:54 -0500

    Update 002.py

--- 002.py
+++ 002.py
@@ -1,9 +1,5 @@
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-# Define array for fibs 
-# Loop through to add next number
-    # Break when latest number is greater than 4M (don't keep)
-
 fibs = [1,2]
 
 # Generate sequence until value exceeds 4,000,000
@@ -12,7 +8,6 @@ while True:
     if next_fib > 4000000:
         break
     fibs.append(next_fib)
-
 
 even_value_fibs = [] 
 
